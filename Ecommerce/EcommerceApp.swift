7e62070c18a7c6bb6713accfcbe79d768065d671
Ronald SG <ronald132@gmail.com>
2021-07-10 20:22:27 +0800

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Ronald on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
